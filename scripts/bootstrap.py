4550d0c7ab5b3d4034f2b864244db6697363f290
Tungstend <74163103+Tungstend@users.noreply.github.com>
2024-11-20 00:22:52 +0800

    Update bootstrap.py

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -13,7 +13,7 @@ import sys
 
 
 def main():
-    gclient_cmd = ('gclient config --name change2dot --unmanaged --no-history '
+    gclient_cmd = ('gclient config --name change2dot --unmanaged '
                    'https://chromium.googlesource.com/angle/angle.git')
     try:
         rc = subprocess.call(gclient_cmd, shell=True)
